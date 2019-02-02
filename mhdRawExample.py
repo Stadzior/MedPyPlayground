49d58f670be590cd18b144263d25c6e710c85dad
Stadzior <Stadzior@gmail.com>
2019-02-02 18:02:05 +0100

    46nd slice

--- mhdRawExample.py
+++ mhdRawExample.py
@@ -12,7 +12,8 @@ for file in filter(lambda x: x.endswith(".raw"), os.listdir(".")):
     # converting to a uint16 numpy array
     img_arr = np.fromstring(img_str, np.uint16)
     # get the first image and plot it
-    im1 = img_arr[0:512*512]
+    im1 = img_arr[512*512*46:512*512*47]
+    
     #im1.byteswap(inplace=True)
     im1 = np.reshape(im1, (512, 512))
     plt.imshow(im1, cmap=plt.cm.gray_r)
