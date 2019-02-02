89962a061afba81eae93a6b13b83b1b56c323bc8
Stadzior <Stadzior@gmail.com>
2019-02-02 17:04:26 +0100

    byteswap

--- mhdRawExample.py
+++ mhdRawExample.py
@@ -2,44 +2,18 @@ import SimpleITK as sitk
 import numpy as np
 from PIL import Image
 import os
-'''
-This funciton reads a '.mhd' file using SimpleITK and return the image array, origin and spacing of the image.
-'''
-def load_itk(filename):
-    # Reads the image using SimpleITK
-    itkimage = sitk.ReadImage(filename)
-
-    # Convert the image to a  numpy array first and then shuffle the dimensions to get axis in the order z,y,x
-    ct_scan = sitk.GetArrayFromImage(itkimage)
-
-    # Read the origin of the ct_scan, will be used to convert the coordinates from world to voxel and vice versa.
-    origin = np.array(list(reversed(itkimage.GetOrigin())))
-
-    # Read the spacing along each dimension
-    spacing = np.array(list(reversed(itkimage.GetSpacing())))
-
-    return ct_scan, origin, spacing
-
-oldMin = -3024
-oldMax = 0
-newMin = 0
-newMax = 255
-
-def arrayToRgbArray(value):
-    oldRange = (oldMax - oldMin)  
-    newRange = (newMax - newMin)  
-    pixelValue = (((value - oldMin) * newRange) / oldRange) + newMin
-    return [pixelValue, pixelValue, pixelValue]
+import matplotlib.pyplot as plt
 
 os.chdir("./mhdraw")
-for file in filter(lambda x: x.endswith(".mhd"), os.listdir(".")):
-    [ctScan, origin, spacing] = load_itk(file)
-    print("-----------------CT SCAN-----------------")
-    (height, width, depth) = ctScan.shape
-    print("Dims: {}, {}, {}".format(height, width, depth))
-    img = Image.frombuffer("I", (512,512), ctScan)
-    img.show()
-    print("-----------------ORIGIN-----------------")
-    print(origin)
-    print("-----------------SPACING-----------------")
-    print(spacing)
+for file in filter(lambda x: x.endswith(".raw"), os.listdir(".")):
+    f = open(file, 'rb')
+    img_str = f.read()
+
+    # converting to a uint16 numpy array
+    img_arr = np.fromstring(img_str, np.uint16)
+    # get the first image and plot it
+    im1 = img_arr[0:512*512]
+    #im1.byteswap(inplace=True)
+    im1 = np.reshape(im1, (512, 512))
+    plt.imshow(im1, cmap=plt.cm.gray_r)
+    plt.show()
